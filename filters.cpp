5627bd550c908b8f8c47fcd7a41e8710229b701d
jehhzacarias <89100514+jehhzacarias@users.noreply.github.com>
2024-02-15 15:12:27 -0300

    Update filters.cpp

--- filters.cpp
+++ filters.cpp
@@ -1,14 +1,45 @@
 #include "filters.h"
 #include "ui_filters.h"
+#include <QMessageBox>
 
 filters::filters(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::filters)
 {
     ui->setupUi(this);
+
+    // Cria o comboBoxFilters
+    comboBoxFilters = new QComboBox(this);
+
+    // Adiciona opções aos filtros no menu suspenso
+    comboBoxFilters->addItem("Portabilidade");
+    comboBoxFilters->addItem("Usabilidade");
+    comboBoxFilters->addItem("Desempenho");
+    comboBoxFilters->addItem("Segurança");
+
+    // Define a geometria do comboBoxFilters
+    comboBoxFilters->setGeometry(40, 60, 211, 26);
+
+    // Conecta o sinal de aplicar filtros ao slot correspondente
+    //connect(ui->applyButton, &QPushButton::clicked, this, &filters::on_applyButton_clicked);
 }
 
 filters::~filters()
 {
     delete ui;
+    delete comboBoxFilters; // Deletando o comboBoxFilters
+}
+
+void filters::on_applyButton_clicked()
+{
+    // Coleta o filtro selecionado
+    QString selectedFilter = comboBoxFilters->currentText();
+
+    // Verifica se algum filtro foi selecionado
+    if (!selectedFilter.isEmpty()) {
+        // Emite sinal com o filtro selecionado
+        emit filtersApplied(QStringList() << selectedFilter);
+    } else {
+        QMessageBox::information(this, "Aplicar Filtros", "Por favor, selecione um filtro.");
+    }
 }
